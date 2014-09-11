fc50a2fe9cc80b89fe799005b8da51fe1a1d9859
Luis Vega <luis@glamdring.local>
2014-09-11 11:25:54 +0800

    Added initial flash system

--- routes/routes.go
+++ routes/routes.go
@@ -19,23 +19,27 @@ var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
 type Page struct {
 	Session     map[interface{}]interface{}
+	Flashes     []interface{}
 	CurrentUser *models.User
 }
 
 func Initialize(r *mux.Router) {
 	r.HandleFunc("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		session, _ := store.Get(r, "teetactoe.com")
+		flashes := session.Flashes()
+		session.Save(r, w)
 
 		t := template.Must(template.ParseFiles("views/layout.html", "views/index.html"))
 
 		user := new(models.User)
-		userId, found := session.Values["user_id"].(int64)
-		if found {
+		userId, ok := session.Values["user_id"].(int64)
+		if ok {
 			user = logic.GetUser(userId)
 		}
 
 		page := Page{
 			Session:     session.Values,
+			Flashes:     flashes,
 			CurrentUser: user}
 
 		t.Execute(w, page)
@@ -47,6 +51,8 @@ func Initialize(r *mux.Router) {
 	})).Methods("GET")
 
 	r.HandleFunc("/signup", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "teetactoe.com")
+
 		password := r.FormValue("password") // This calls r.ParseForm() already
 
 		user, formErrors := forms.Signup.Validate(r.Form)
@@ -56,12 +62,14 @@ func Initialize(r *mux.Router) {
 		}
 
 		if err := logic.CreateUser(user, password); err != nil {
-			log.Fatal(err)
+			switch {
+			case err.Error() == "A user with that email already exists!":
+				session.AddFlash(err.Error())
+				session.Save(r, w)
+			default:
+				log.Fatal(err)
+			}
 		}
-
-		session, _ := store.Get(r, "teetactoe.com")
-		session.Values["user_id"] = user.Id
-		session.Save(r, w)
 
 		http.Redirect(w, r, "/", 303)
 	})).Methods("POST")
