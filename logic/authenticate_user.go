8694c0f4cef51129d1980e16f45456a43d85bb3c
luiscvega <luiscvega@gmail.com>
2014-10-14 17:08:05 +0800

    Play around with error handling

--- logic/authenticate_user.go
+++ logic/authenticate_user.go
@@ -8,16 +8,19 @@ import (
 	"./../models"
 )
 
+var ErrUserNotFound = errors.New("User with those credentials not found!")
+
 func AuthenticateUser(email, password string) (user *models.User, err error) {
 	user = new(models.User)
 
 	err = DB.QueryRow("SELECT id, email, first_name, last_name FROM users WHERE email = $1 AND crypted_password = $2", email, password).Scan(&user.Id, &user.Email, &user.FirstName, &user.LastName)
-	switch {
-	case err == sql.ErrNoRows:
-		err = errors.New("User with those credentials not found!")
-	case err != nil:
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, ErrUserNotFound
+		}
+
 		log.Fatal(err)
 	}
 
-	return
+	return user, nil
 }
